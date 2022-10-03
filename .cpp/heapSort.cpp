c9c44d24ca36e3540ba59fc712f8b76be7b2e483
Sandipan Chatterjee <79105018+sandy3002@users.noreply.github.com>
2022-10-03 17:44:13 +0530

    added heap sort

---
+++ .cpp/heapSort.cpp
@@ -0,0 +1,62 @@
+// C++ program for implementing Heap Sort
+
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int N, int i)
+{
+
+	int largest = i;
+
+	int l = 2 * i + 1;
+
+	int r = 2 * i + 2;
+
+	if (l < N && arr[l] > arr[largest])
+		largest = l;
+
+	if (r < N && arr[r] > arr[largest])
+		largest = r;
+
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+
+		heapify(arr, N, largest);
+	}
+}
+
+void heapSort(int arr[], int N)
+{
+
+	for (int i = N / 2 - 1; i >= 0; i--)
+		heapify(arr, N, i);
+
+	for (int i = N - 1; i > 0; i--) {
+
+		swap(arr[0], arr[i]);
+
+		heapify(arr, i, 0);
+	}
+}
+
+void printArray(int arr[], int N)
+{
+	for (int i = 0; i < N; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter array size: ";
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	
+	heapSort(arr, n);
+
+	cout << "Sorted array is \n";
+	printArray(arr, n);
+}
